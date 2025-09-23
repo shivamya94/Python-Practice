dafa11f46c17cbc46d4467b9c0cc87363d677dc6
Shivamya94 <yadavshivam90440@gmail.com>
2025-09-23 15:57:51 +0530

    new code

--- Loggin_python.py
+++ Loggin_python.py
@@ -1,29 +1,40 @@
+
 import logging
 import os
 
-#create logger string
-logging_str = "[%(asctime)s : %(levelname)s : %(module)s : %(message)s]"
+#create a logger string
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
 
-log_folder = "Logs"
-log_file = "running_log.log"
+log_folder = "logs"
+log_file = "logs/running_log.log"
 
-os.mkdirs(log_folder,exist_ok = True)
-
-
+os.makedirs(log_folder, exist_ok=True)
 
 
+logging.basicConfig(
+    level= logging.INFO,
+    format= logging_str,
+
+    handlers=[
+        logging.FileHandler(log_file)
+    ]
+)
+
+logger = logging.getLogger("mylog")
+
+
+def division(a,b):
+    try:
+        out = a/b
+        return out
+    except Exception as e:
+        logger.info(e)
+        addds = a + b
+        print(addds)
 
 
 
 
 
-
-# def add_number(a,b):
-#     out = a + b
-#     print("Succesully printed")
-
-#     return out
-
-
-# num = add_number(3,4)
-# print(num)
+out = division(4,0)
+print(out)
